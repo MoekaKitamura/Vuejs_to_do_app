f7a799c191799c16b427b4427b56ca5820eef8b8
moeka kitamura <moeka.k76@gmail.com>
2022-10-23 19:19:42 +0900

    close issue

--- src/views/IssueList.vue
+++ src/views/IssueList.vue
@@ -2,12 +2,12 @@
   <div>
     <el-button type="success" round @click="getIssues()">issue取得</el-button>
     <el-row :gutter="12">
-      <el-col :span="12" v-for="issue in issues" :key="issue.id">
+      <el-col :span="12" v-for="( issue, index ) in issues" :key="issue.id">
         <el-card class="box-card" shadow="hover" style="margin: 5px 0;">
           <el-row :gutter="12">
             <el-col :span="21">{{ issue.title }}</el-col>
             <el-col :span="3">
-              <el-button type="success" icon="el-icon-check" circle></el-button>
+              <el-button @click="closeIssue(index)" type="success" icon="el-icon-check" circle></el-button>
             </el-col>
           </el-row>
         </el-card>
@@ -41,7 +41,18 @@ export default {
         .then((res) => {
           this.issues = res.data;
       })
-    }
+    },
+    closeIssue(index){
+      const target = this.issues[index]
+      client.patch(`/issues/${target.number}`,
+          {
+            state: 'closed'
+          },
+        )
+        .then(() => {
+          this.issues.splice(index, 1)
+      })
+    },
   }
 }
 </script>
